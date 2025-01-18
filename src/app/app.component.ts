95a28dad8fcdd22329bf02ad6c409d2c032ee98a
Luiggi Plasencia <luiggi.plasencia@uvesolutions.com>
2025-01-18 02:10:52 +0100

    Config MF shell

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import PubSub from 'pubsub-js';
+
+interface ICommonProduct {
+	price: number;
+	name: string;
+}
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+	constructor() {}
+	count = 0;
+	private _products: ICommonProduct[] = [];
+
+	ngOnInit(): void {
+		this.getCount();
+		PubSub.subscribe('products', (_messsage, data) => {
+			this._products.push(data as unknown as ICommonProduct);
+			this.count++;
+			localStorage.setItem('products', JSON.stringify(this._products));
+		});
+	}
+
+	getCount() {
+		const productsStorage = localStorage.getItem('products');
+
+		if (productsStorage) {
+			const product = JSON.parse(productsStorage) as ICommonProduct[];
+			this.count = product.length;
+		}
+	}
+}
