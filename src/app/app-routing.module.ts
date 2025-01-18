5e0aa107722edef254f98f51ff45cbc53884e443
Luiggi Plasencia <luiggi.plasencia@uvesolutions.com>
2025-01-18 02:16:47 +0100

    Use hash routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,28 +3,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-	{
-		path: '',
-		loadChildren: () =>
-			loadRemoteModule({
-				type: 'module',
-				remoteEntry: 'http://localhost:4201/remoteEntry.js',
-				exposedModule: './ProductsModule',
-			}).then((m) => m.ProductsModule),
-	},
-	{
-		path: 'payment',
-		loadComponent: () =>
-			loadRemoteModule({
-				type: 'module',
-				remoteEntry: 'http://localhost:4202/remoteEntry.js',
-				exposedModule: './PaymentComponent',
-			}).then((m) => m.PaymentComponent),
-	},
+  {
+    path: '',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './ProductsModule',
+      }).then((m) => m.ProductsModule),
+  },
+  {
+    path: 'payment',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './PaymentComponent',
+      }).then((m) => m.PaymentComponent),
+  },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
