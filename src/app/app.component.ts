b5e19d94c0d30f0cf331d469ed420318ccad5a0d
Luiggi Plasencia <luiggi.plasencia@uvesolutions.com>
2025-01-19 02:10:28 +0100

    Migrate angular and module federation 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ interface ICommonProduct {
 }
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
 	constructor() {}
