e9175f92628591d27934f92b7ff0bc1ce93d69eb
Luiggi Plasencia <luiggi.plasencia@uvesolutions.com>
2025-01-20 22:52:44 +0100

    Subiendo cambios

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  mfManifest: '/assets/mf.manifest-dev.json',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-// import('./bootstrap').catch((err) => console.error(err));
-
 import { loadManifest } from '@angular-architects/module-federation';
+import { environment } from './environments/environment';
 
-loadManifest('/assets/mf.manifest.json', true)
+console.log(environment);
+
+// loadManifest('/assets/mf.manifest-dev.json', true)
+loadManifest(environment.mfManifest, true)
   .catch((err) => console.error(err))
   .then((_) => import('./bootstrap'))
   .catch((err) => console.error(err));
