ecbd83336a5314f94595ea22a68de4d14886bc6d
Luiggi Plasencia <luiggi.plasencia@uvesolutions.com>
2025-01-20 22:59:48 +0100

    Config shell

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { environment } from './environments/environment';
 
 console.log(environment);
 
-// loadManifest('/assets/mf.manifest-dev.json', true)
-loadManifest(environment.mfManifest, true)
+loadManifest('/assets/mf.manifest-prod.json', true)
+  // loadManifest(environment.mfManifest, true)
   .catch((err) => console.error(err))
   .then((_) => import('./bootstrap'))
   .catch((err) => console.error(err));
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,1 +1,6 @@
-module.exports = require('./webpack.config');
+const config = require("./webpack.config");
+
+// config.output.publicPath = "http://127.0.0.1:8080/";
+config.output.publicPath = "https://mf-shell.netlify.app/";
+
+module.exports = config;
